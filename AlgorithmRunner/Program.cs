22154914c91d16820d8833fb485dc6a1d28d72a2
Jason Dentler <jasondentler@gmail.com>
2011-03-05 11:55:20 -0600

    Time pattern parser

--- AlgorithmRunner/Program.cs
+++ AlgorithmRunner/Program.cs
@@ -1,25 +1,43 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace AlgorithmRunner
 {
     class Program
     {
 
-        private const string RawData = @"C:\Projects\Room Scheduling Algorithm\RawData";
+        static Program()
+        {
+            var workingDirectory = Process.GetCurrentProcess().StartInfo.WorkingDirectory;
+            RawDataDirectory = Path.Combine(workingDirectory, @"..\..\..\RawData");
+        }
+
+        private static readonly string RawDataDirectory;
 
         static void Main(string[] args)
         {
             var p = new Program();
-            p.BuildConflictWeightFile();
+            p.BuildTimePatterns();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
         }
 
         private void BuildConflictWeightFile()
         {
             var weights = new ConflictWeightsGenerator(
-                Path.Combine(RawData, "Sections.Fall2010.xml"),
-                Path.Combine(RawData, "Sections.xml"),
-                Path.Combine(RawData, "StudentSections.Fall2010.xml"));
-            weights.Generate(Path.Combine(RawData, "ConflictWeights.xml"));
+                Path.Combine(RawDataDirectory, "Sections.Fall2010.xml"),
+                Path.Combine(RawDataDirectory, "Sections.xml"),
+                Path.Combine(RawDataDirectory, "StudentSections.Fall2010.xml"));
+            weights.Generate(Path.Combine(RawDataDirectory, "ConflictWeights.xml"));
+        }
+
+        private void BuildTimePatterns()
+        {
+            var loader = new TimePatternLoader(
+                Path.Combine(RawDataDirectory, "SectionMeetings.xml"));
+            var sw = loader.Load();
+            File.WriteAllText(Path.Combine(RawDataDirectory, "Patterns.txt"), sw.ToString());
         }
 
     }
---
+++ AlgorithmRunner/TimePattern.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmRunner
+{
+
+    public struct TimePattern
+    {
+        public DaysOfWeekBitMask Days;
+        public DateTime Start;
+        public DateTime End;
+
+        public TimePattern(DaysOfWeekBitMask days, DateTime start, DateTime end)
+        {
+            Days = days;
+            Start = start;
+            End = end;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} to {2}",
+                                 Days.Convert(),
+                                 Start.ToShortTimeString(),
+                                 End.ToShortTimeString());
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = (TimePattern) obj;
+            return Days == other.Days &&
+                   Start == other.Start &&
+                   End == other.End;
+        }
+
+    }
+
+    [Flags]
+    public enum DaysOfWeekBitMask
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 8,
+        Thursday = 16,
+        Friday = 32,
+        Saturday = 64
+    }
+
+    public static class DaysOfWeekBitMaskExtensions
+    {
+        public static DaysOfWeekBitMask Convert(IEnumerable<string> days)
+        {
+            return Convert(days.Select(Convert));
+        }
+
+        public static DaysOfWeekBitMask Convert(IEnumerable<DaysOfWeekBitMask> days)
+        {
+            return days.Aggregate<DaysOfWeekBitMask, DaysOfWeekBitMask>
+                (0, (current, day) => current | day);
+        }
+
+        public static DaysOfWeekBitMask Convert(string day)
+        {
+            switch (day.ToUpperInvariant())
+            {
+                case "SU":
+                    return DaysOfWeekBitMask.Sunday;
+                case "M":
+                    return DaysOfWeekBitMask.Monday;
+                case "T":
+                case "TU":
+                    return DaysOfWeekBitMask.Tuesday;
+                case "W":
+                    return DaysOfWeekBitMask.Wednesday;
+                case "TH":
+                    return DaysOfWeekBitMask.Thursday;
+                case "F":
+                    return DaysOfWeekBitMask.Friday;
+                case "S":
+                case "SA":
+                    return DaysOfWeekBitMask.Saturday;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public static string Convert(this DaysOfWeekBitMask days)
+        {
+            var result = new StringBuilder();
+            if (days.Contains(DaysOfWeekBitMask.Sunday))
+                result.Append("Su");
+            if (days.Contains(DaysOfWeekBitMask.Monday))
+                result.Append("M");
+            if (days.Contains(DaysOfWeekBitMask.Tuesday))
+                result.Append("Tu");
+            if (days.Contains(DaysOfWeekBitMask.Wednesday))
+                result.Append("W");
+            if (days.Contains(DaysOfWeekBitMask.Thursday))
+                result.Append("Th");
+            if (days.Contains(DaysOfWeekBitMask.Friday))
+                result.Append("F");
+            if (days.Contains(DaysOfWeekBitMask.Saturday))
+                result.Append("Sa");
+            return result.ToString();
+        }
+
+        public static bool Contains(this DaysOfWeekBitMask days, DaysOfWeekBitMask day)
+        {
+            return (days & day) == day;
+        }
+
+    }
+
+
+}
